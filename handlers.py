81ba720238c49f9c923a4f0623af3014055178aa
Ron <ronmadlal@gmail.com>
2018-10-21 23:56:59 +0300

    Major refactor: move commands to new CommandHandler class.

--- handlers.py
+++ handlers.py
@@ -4,6 +4,7 @@ import re
 import socket
 import subprocess
 from datetime import datetime
+from typing import Tuple
 
 import pyperclip
 import requests
@@ -42,20 +43,9 @@ class TwitchIRCHandler(object):
     def __init__(self):
         self._sock = socket.socket()
         self._ffz_emote_cache = []
-        self._init_regexes()
-
-    def _init_regexes(self):
         self._re_end = re.compile(r'^:\w+\.tmi\.twitch\.tv 366 \w+ #(?P<channel_name>\w+) :End of /NAMES list$')
         self._re_message = re.compile(r'^:(?P<user>\w+)!\1@\1\.tmi\.twitch\.tv '
                                       rf'PRIVMSG #{MY_USERNAME} :(?P<message>.+)$')
-
-        # Pyramid command
-        part_cmd = r'!pyramid'
-        part_text = r'(?P<text>\S(.*\S)?)'
-        # !pyramid <text>
-        self._re_pyramid_def = re.compile(rf'^{part_cmd} +{part_text}.*$')
-        # !pyramid <size> <text>
-        self._re_pyramid_with_size = re.compile(rf'^{part_cmd} +(?P<size>\d+) +{part_text}.*$')
 
     def connect(self):
         try:
@@ -99,7 +89,7 @@ class TwitchIRCHandler(object):
             print(f'Connection reset: {err.strerror}')
             return
 
-    def say(self, msg):
+    def say(self, msg: str):
         try:
             msg = msg.replace('\n', ' ')
             self._sock.send(bytes(f'PRIVMSG #{MY_USERNAME} :{msg}\r\n', 'utf-8'))
@@ -107,57 +97,8 @@ class TwitchIRCHandler(object):
         except socket.error as err:
             print(f'Connection reset: {err.strerror}')
 
-    def action(self, msg):
+    def action(self, msg: str):
         self.say(msg if msg.startswith('/me ') else f'/me {msg}')
-
-    # Commands
-    def _send_pyramid(self, text, size=3):
-        if size not in range(1, 8):
-            self.action('Pyramid size must be between 1 and 7.')
-            return
-        for i in range(2 * size - 1):
-            block = [text] * (i + 1 if i < size else 2 * size - (i + 1))
-            self.say(' '.join(block))
-
-    def pyramid(self, msg):
-        command = self._re_pyramid_with_size.match(msg) or self._re_pyramid_def.match(msg)
-        if not command:
-            self.action('Usage: !pyramid [<size>] <text>')
-            return
-        if 'size' in command.groupdict():   # Pyramid size was specified
-            self._send_pyramid(command['text'], int(command['size']))
-            return
-        self._send_pyramid(command['text'])
-
-    def random_emote(self):
-        if not self._ffz_emote_cache:
-            with requests.get(f'http://api.frankerfacez.com/v1/room/{MY_USERNAME}') as response:
-                data = response.json()
-            set_num = data['room']['set']
-            emoticons = data['sets'][str(set_num)]['emoticons']
-            self._ffz_emote_cache = [emote['name'] for emote in emoticons]
-
-        self.say(random.choice(self._ffz_emote_cache))
-
-    def now_playing(self):
-        foobar2k = r'C:\Program Files (x86)\foobar2000\foobar2000.exe'
-        message = 'N/A'
-
-        # Verify that foobar2k is running
-        tasks = subprocess.check_output(['tasklist', '/FO', 'CSV'], shell=True, universal_newlines=True)
-        tasks = (s.split(',')[0] for s in tasks.splitlines())
-        if '"foobar2000.exe"' not in tasks:
-            self.action(message)
-            return
-
-        # Run a foobar2k command that copies the currently playing track to clipboard
-        prev_clip = pyperclip.paste()   # Save current clipboard content to restore later
-        pyperclip.copy('')              # Clear clipboard first, so we can check for failure
-        subprocess.run([foobar2k, '/runcmd-playlist=Copy name'])
-        track = pyperclip.paste()
-        pyperclip.copy(prev_clip)
-        message = f'Now playing: {track}' if track else message
-        self.action(message)
 
 
 class TwitchAPIHandler(TwitchClient):
@@ -166,29 +107,26 @@ class TwitchAPIHandler(TwitchClient):
         super(TwitchAPIHandler, self).__init__(CLIENT_ID, API_TOKEN)
         self._clip_cache = []
 
-    def update_game(self, game):
+    def update_game(self, game: str):
         self.channels.update(CHANNEL_ID, game=game)
 
-    def update_title(self, title):
+    def update_title(self, title: str):
         self.channels.update(CHANNEL_ID, status=title)
 
     # Commands
     def highlight(self):
         stream = self.streams.get_stream_by_user(CHANNEL_ID)
         if not stream:
-            return 'Unexpected error'
+            return ''
         delta = (datetime.utcnow() - stream['created_at']).seconds
         timestamp = '{}:{:02}'.format(delta // 60 ** 2, delta // 60 % 60)
         with open('timestamps.txt', 'a') as ts_file:
             ts_file.write(timestamp + '\n')
-        return f'Timestamp saved! [{timestamp}]'
+        return timestamp
 
     def random_clip(self):
         if not self._clip_cache:
-            try:
-                self._clip_cache = self.clips.get_top(channel=MY_USERNAME, limit=50, period='all')
-            except requests.RequestException:
-                return 'Unexpected error'
+            self._clip_cache = self.clips.get_top(channel=MY_USERNAME, limit=50, period='all')
         clip = pop_random_item(self._clip_cache)
         return clip['url']
 
@@ -213,3 +151,109 @@ class JokeHandler(object):
             self._fetch_jokes()
         joke = pop_random_item(self._joke_cache)
         return ' '.join(joke).replace('\n', ' ')
+
+
+class CommandHandler(object):
+
+    def __init__(self, _irc_client: TwitchIRCHandler):
+        self._irc_client = _irc_client
+        self._twitch_api_handler = TwitchAPIHandler()
+        self._joke_handler = JokeHandler()
+        self._ffz_emote_cache = []
+        self._re = re.compile(r'^!(?P<cmd>\w+)(?P<args>( +\S+)*)')
+
+    def __call__(self, msg: str):
+        m = self._re.match(msg)
+        if not m:
+            return
+        cmd, args = m['cmd'], m['args'].split()
+        method = getattr(self, 'cmd_' + cmd, self.cmd_help)
+        try:
+            method(*self._parse_args_for_method(method, *args))
+        except ValueError as e:
+            self._irc_client.action(str(e))
+
+    def _parse_args_for_method(self, method, *args) -> Tuple:
+        if method in (self.cmd_game, self.cmd_title):
+            return ' '.join(args),
+        if method == self.cmd_pyramid:
+            return self._parse_pyramid_args(*args)
+        return args
+
+    def cmd_help(self):
+        cmd_list = ['!' + m[4:] for m in dir(self) if m.startswith('cmd_')]
+        self._irc_client.action(f"Commands: {', '.join(cmd_list)}")
+
+    def cmd_game(self, game: str):
+        self._twitch_api_handler.update_game(game)
+
+    def cmd_title(self, title: str):
+        self._twitch_api_handler.update_title(title)
+
+    def cmd_highlight(self):
+        timestamp = self._twitch_api_handler.highlight()
+        self._irc_client.action(f'Timestamp saved! [{timestamp}]' if timestamp else 'Unexpected error')
+
+    def cmd_np(self):
+        foobar2k = r'C:\Program Files (x86)\foobar2000\foobar2000.exe'
+
+        # Verify that foobar2k is running
+        tasks = subprocess.check_output(['tasklist', '/FO', 'CSV'], shell=True, universal_newlines=True)
+        tasks = (s.split(',')[0] for s in tasks.splitlines())
+        if '"foobar2000.exe"' not in tasks:
+            self._irc_client.action('N/A')
+            return
+
+        # Run a foobar2k command that copies the currently playing track to clipboard
+        prev_clip = pyperclip.paste()  # Save current clipboard content to restore later
+        pyperclip.copy('')  # Clear clipboard first, so we can check for failure
+        subprocess.run([foobar2k, '/runcmd-playlist=Copy name'])
+        track = pyperclip.paste()
+        pyperclip.copy(prev_clip)
+        self._irc_client.action(f'Now playing: {track}' if track else 'N/A')
+
+    def cmd_joke(self):
+        joke = self._joke_handler.random_joke()
+        self._irc_client.say(joke)
+
+    def cmd_emote(self):
+        if not self._ffz_emote_cache:
+            try:
+                with requests.get(f'http://api.frankerfacez.com/v1/room/{MY_USERNAME}') as response:
+                    response.raise_for_status()
+                    data = response.json()
+                set_num = data['room']['set']
+                emoticons = data['sets'][str(set_num)]['emoticons']
+                self._ffz_emote_cache = [emote['name'] for emote in emoticons]
+            except requests.RequestException:
+                self._irc_client.action('Unexpected error')
+
+        self._irc_client.say(random.choice(self._ffz_emote_cache))
+
+    def cmd_clip(self):
+        try:
+            clip = self._twitch_api_handler.random_clip()
+            self._irc_client.say(clip)
+        except requests.RequestException:
+            self._irc_client.action('Unexpected error')
+
+    def cmd_pyramid(self, text, size):
+        pyramid = [' '.join([text] * (i + 1 if i < size else 2 * size - (i + 1)))
+                   for i in range(2 * size - 1)]
+        for block in pyramid:
+            self._irc_client.say(block)
+
+    @staticmethod
+    def _parse_pyramid_args(*args) -> Tuple[str, int]:
+        # !pyramid <size: number from 1 to 7> <text: string>
+        # or !pyramid <text: string> (in this case, the pyramid will be of size 3)
+        if not args:
+            raise ValueError('Usage: !pyramid [<size>] <text>')
+        size = 3
+        if len(args) > 1 and str(args[0]).isnumeric():
+            size = int(args[0])
+            if size not in range(1, 8):
+                raise ValueError('Pyramid size must be between 1 and 7.')
+            args = args[1:]
+        text = ' '.join(args)
+        return text, size
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sys
 import threading
 
 import requests
 
-from handlers import MY_USERNAME, BOT_USERNAME, TwitchIRCHandler, TwitchAPIHandler, JokeHandler
+from handlers import MY_USERNAME, BOT_USERNAME, TwitchIRCHandler, CommandHandler
 
 
 def fetch_viewers(interval):
@@ -41,8 +40,7 @@ def send_random_emotes(irc_client, interval):
 
 def main():
     irc_client = TwitchIRCHandler()
-    twitch_api_handler = TwitchAPIHandler()
-    joke_handler = JokeHandler()
+    command_handler = CommandHandler(irc_client)
 
     cancel_fetch_viewers = fetch_viewers(60)
     cancel_send_random_emotes = send_random_emotes(irc_client, 60*10)
@@ -50,38 +48,6 @@ def main():
     def cancel_repeating_threads():
         cancel_fetch_viewers()
         cancel_send_random_emotes()
-
-    commands = {
-        'PogChamp': lambda: irc_client.say('ChampPog'),
-        'ChampPog': lambda: irc_client.say('PogChamp'),
-        '!help': lambda: irc_client.action(f"Commands: {' '.join(list(commands.keys())[3:])}"),
-        '!highlight': lambda: irc_client.action(twitch_api_handler.highlight()),
-        '!np': lambda: irc_client.now_playing(),
-        '!pyramid': lambda: irc_client.action('Usage: !pyramid [<size>] <text>'),
-        '!joke': lambda: irc_client.say(joke_handler.random_joke()),
-        '!emote': lambda: irc_client.random_emote(),
-        '!clip': lambda: irc_client.action(twitch_api_handler.random_clip())
-    }
-
-    def other_command(msg):
-        # Commands with arguments (e.g !pyramid) and owner-only commands are handled here
-        if msg.startswith('!pyramid '):
-            irc_client.pyramid(msg)
-            return
-        if username != MY_USERNAME:
-            if msg.startswith('!'):
-                commands['!help']()
-            return
-        # Channel owner only
-        if msg.startswith('!game '):
-            twitch_api_handler.update_game(msg[len('!game '):])
-        elif msg.startswith('!title '):
-            twitch_api_handler.update_title(msg[len('!title '):])
-        elif msg == '!goaway':
-            irc_client.action('Bye')
-            irc_client.disconnect()
-            cancel_repeating_threads()
-            sys.exit(0)
 
     if not irc_client.connect():
         cancel_repeating_threads()
@@ -96,7 +62,7 @@ def main():
             print(f'{username}: {message}')
             if username == BOT_USERNAME:
                 continue
-            commands.get(message, lambda: other_command(message))()
+            command_handler(message)
 
 
 if __name__ == '__main__':
